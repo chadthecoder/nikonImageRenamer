9fc5a3832573a09bde25386e336bb0006b67cbf7
chadthecoder <mchadamschad@outlook.com>
2020-01-20 18:06:18 -0800

    renamed poorly named variables

--- main.cpp
+++ main.cpp
@@ -3,40 +3,40 @@
 #include <algorithm>
 #include <filesystem>
 
-namespace fs = std::filesystem;
+//namespace fs = std::filesystem;
 
 int main() {
-    std::string path = fs::current_path();
+    std::string path = std::filesystem::current_path();
     //path += "/pics";
     std::cout << path << std::endl << std::endl;
-    int test = 0;
-    for (const auto & entry : fs::directory_iterator(path))
+    int numInt = 0;
+    for (const auto & entry : std::filesystem::directory_iterator(path))
     {
-        std::string fun = entry.path();//.filename();
-        std::string fun2 = fun;
-        std::transform(fun2.begin(), fun2.end(), fun2.begin(), ::tolower);
-        //std::string fun2 = fs::file_status();//entry.path().extension();
-        std::cout << fun  << std::endl;
-        if(fun2.rfind(".jpg")!=-1)
+        std::string fileName1 = entry.path();//.filename();
+        std::string fileName2 = fileName1;
+        std::transform(fileName2.begin(), fileName2.end(), fileName2.begin(), ::tolower);
+        //std::string fileName2 = fs::file_status();//entry.path().extension();
+        std::cout << fileName1  << std::endl;
+        if(fileName2.rfind(".jpg")!=-1)
         {
-            std::string num = std::to_string(test);
-            while(num.length() < 4)
+            std::string numString = std::to_string(numInt);
+            while(numString.length() < 4)
             {
-                num = "0" + num;
+                numString = "0" + numString;
             }
-            std::cout << std::rename(fun.c_str(), (path + "/DSCN" + num + ".jpg").c_str()) << std::endl;
-            std::cout << (path + "/DSCN" + num + ".JPG").c_str() << std::endl;
-            test++;
+            std::cout << std::rename(fileName1.c_str(), (path + "/DSCN" + numString + ".jpg").c_str()) << std::endl;
+            std::cout << (path + "/DSCN" + numString + ".JPG").c_str() << std::endl;
+            numInt++;
         }
-        else if(fun2.rfind(".png")!=-1)
+        else if(fileName2.rfind(".png")!=-1)
         {
-            std::string num = std::to_string(test);
-            while(num.length() < 4)
+            std::string numString = std::to_string(numInt);
+            while(numString.length() < 4)
             {
-                num = "0" + num;
+                numString = "0" + numString;
             }
-            std::cout << std::rename(fun.c_str(), (path + "/DSCN" + num + ".PNG").c_str()) << std::endl;
-            test++;
+            std::cout << std::rename(fileName1.c_str(), (path + "/DSCN" + numString + ".PNG").c_str()) << std::endl;
+            numInt++;
         }
         else
         {
